5cb6e35869ba1223c9c1ced7e24dc5aa2673bac6
NiinaKurita <109599968+NiinaKurita@users.noreply.github.com>
2022-11-09 13:59:39 +0900

    チェックテスト　直し

--- JavaStudy/チェックテスト/4-1/src/check/Main.java
+++ JavaStudy/チェックテスト/4-1/src/check/Main.java
@@ -2,44 +2,26 @@ package check;
 import constants.Constants;
 
 public class Main{
-
-		private static String firstName;
-		private static String lastName;
-		
-		
-		public Main(String firstName,String lastName) {
-			this.firstName = "Niina";
-			this.lastName = "Kurita";
-		}
-		
-		public String getFirstName() {
-			return firstName;
-		}
-		
-		public String getLastName() {
-			return lastName;
-		}
-		
-		private String printName() {
-			return firstName+" "+lastName;
-		}
-		
-		public String getPrintName() {
-			return this.printName();
-		}
-		
-		public void sample() {
+	private String firstName;
+	private String lastName;
+	
+	
+	public Main(String firstName,String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	private void printName() {
 		System.out.println("printNameメソッド　→　"+ firstName + lastName );
-		}
-		
+	}
 
-			public static void main(String[] args) { 
+	public static void main(String[] args) { 
+		Main main = new Main("Niina","Kurita");
+		main.printName();
+		Pet pet1 = new Pet(Constants.CHECK_CLASS_JAVA,Constants.CHECK_CLASS_HOGE);
+		pet1.introduce();
+		RobotPet robotpet = new RobotPet(Constants.CHECK_CLASS_R2D2,Constants.CHECK_CLASS_LUKE);
+		robotpet.introduce();
 			
-			Main main = new Main("七海","真弥");
-			
-			Pet pet1 = new Pet(Constants.CHECK_CLASS_JAVA,Constants.CHECK_CLASS_HOGE);
-			RobotPet robotpet = new RobotPet(Constants.CHECK_CLASS_R2D2,Constants.CHECK_CLASS_LUKE);
-		
-			
-			}
+	}
 }
